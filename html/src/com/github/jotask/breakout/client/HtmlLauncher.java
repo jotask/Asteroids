546939a4fcd39410d44bc6abf8585654c9da136b
Jota <jota_sk@hotmail.com>
2017-07-31 13:19:06 +0100

    updated full screen

--- html/src/com/github/jotask/breakout/client/HtmlLauncher.java
+++ html/src/com/github/jotask/breakout/client/HtmlLauncher.java
@@ -1,15 +1,33 @@
 package com.github.jotask.breakout.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.github.jotask.breakout.asteroid.Asteroids;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
+        public void onModuleLoad () {
+                super.onModuleLoad();
+                com.google.gwt.user.client.Window.addResizeHandler(new ResizeHandler() {
+                        public void onResize(ResizeEvent ev) {
+//                                Gdx.graphics.setDisplayMode(ev.getWidth(),ev.getHeight(), false);
+                                Gdx.graphics.setWindowedMode(ev.getWidth(), ev.getHeight());
+                        }
+                });
+        }
+
+        @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                int height = com.google.gwt.user.client.Window.getClientHeight();
+                int width = com.google.gwt.user.client.Window.getClientWidth();
+                GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(width, height);
+                return cfg;
+//                return new GwtApplicationConfiguration(480, 320);
         }
 
         @Override
